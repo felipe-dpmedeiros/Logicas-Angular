74caa90c7a6be3ec5c6ce705fd89e9256ce168eb
felipe-dpmedeiros <felipe.dellapaschoa@gmail.com>
2025-04-10 11:58:38 -0300

    Estrutura inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InterpolacaoComponent } from './components/interpolacao/interpolacao.component';
+import { ControlFlowComponent } from './components/control-flow/control-flow.component';
+import { Exercicio1Component } from './components/exercicio1/exercicio1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InterpolacaoComponent, ControlFlowComponent, Exercicio1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
